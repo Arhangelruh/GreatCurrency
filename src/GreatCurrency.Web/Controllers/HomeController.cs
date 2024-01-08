6f60e4efac072b207b5e7f729dcaa293da4e946a
Ruh <ruh@tut.by>
2024-01-08 17:14:25 +0300

    init: add bankfilter

--- src/GreatCurrency.Web/Controllers/HomeController.cs
+++ src/GreatCurrency.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GreatCurrency.BLL.Interfaces;
+using GreatCurrency.BLL.Models;
 using GreatCurrency.Web.Services;
 using GreatCurrency.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -38,7 +39,7 @@ namespace GreatCurrency.Web.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(int? page, int? pagesize, int? bankId, RequestViewModel requestData)                
+        public async Task<IActionResult> Index(int? page, int? pagesize, RequestViewModel requestData)                
         {
             int pageSize = (int)(pagesize == null ? 30 : pagesize);
             ViewData["PageSize"] = pageSize;
@@ -61,28 +62,33 @@ namespace GreatCurrency.Web.Controllers
                 return View();
             }            
 
-            ViewBag.CityList = list;            
+            ViewBag.CityList = list;
+
+            var mainbank = await _bankService.GetBankByNameAsync(_getParameters.MainBank);
+            if (mainbank is null)
+            {
+                ViewBag.ErrorMessage = "Не найден банк по умолчанию указанный в конфигурации.";
+                ViewBag.ErrorTitle = "Ошибка";
+                return View("~/Views/Error/Error.cshtml");
+            }
+
+           // List<BankDto> bankList = [];
+
+            var banks = await _bankService.GetAllBanksAsync();
             
+            ViewBag.BankList = banks;
+
             DateTime now = DateTime.Now;
 
             DateTime firstDate = (DateTime)(!requestData.startDate.HasValue ? now.Date : requestData.startDate);
             DateTime secondDate = (DateTime)(!requestData.endDate.HasValue ? now.Date.AddDays(1) : requestData.endDate);
             var cityId = (int)(requestData.cityId == null ? cities.First().Id : requestData.cityId);
+            var bankId = (int)(requestData.bankId == null ? mainbank.Id : requestData.bankId);
 
             ViewData["StartData"] = firstDate;
             ViewData["EndData"] = secondDate;
-            ViewData["CurrentCity"] = cityId;            
-
-            if (bankId is null)
-            {
-                var bank = await _bankService.GetBankByNameAsync(_getParameters.MainBank);
-                if (bank is null) {
-                    ViewBag.ErrorMessage = "В базе не найден банк указанный в конфигурации.";
-                    ViewBag.ErrorTitle = "Ошибка";
-                    return View("~/Views/Error/Error.cshtml");
-                }
-                    bankId = bank.Id;                
-            }
+            ViewData["CurrentCity"] = cityId;
+            ViewData["CurrentBank"] = bankId;
 
             var bestRates = await _bestCurrencyService.GetCurrenciesByTimeAsync(firstDate, secondDate,cityId);
 
@@ -91,7 +97,7 @@ namespace GreatCurrency.Web.Controllers
                 return View();
             }
 
-            var requestViewModel = new RequestViewModel { startDate = firstDate, endDate = secondDate, cityId=cityId };
+            var requestViewModel = new RequestViewModel { startDate = firstDate, endDate = secondDate, cityId=cityId, bankId = bankId };
             var modelsBestRates = new List<BestRatesViewModel>();
 
             foreach (var rate in bestRates)
--- src/GreatCurrency.Web/ViewModels/RequestViewModel.cs
+++ src/GreatCurrency.Web/ViewModels/RequestViewModel.cs
@@ -2,9 +2,24 @@
 {
     public class RequestViewModel
     {
+        /// <summary>
+        /// start date.
+        /// </summary>
         public DateTime? startDate { get; set; }
+
+        /// <summary>
+        /// End date.
+        /// </summary>
         public DateTime? endDate { get; set; }
 
+        /// <summary>
+        /// City id.
+        /// </summary>
         public int? cityId { get; set; }
+
+        /// <summary>
+        /// Bank id.
+        /// </summary>
+        public int? bankId { get; set; }
     }
 }
