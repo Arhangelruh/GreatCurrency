eeb95499d1793f677940d6ce65a30934d42c9819
Ruh <ruh@tut.by>
2024-10-19 12:31:06 +0300

    init: Add Dto models for services currency

---
+++ src/GreatCurrency.BLL/Models/CurrencyServiceDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatCurrency.BLL.Models
+{
+	public class CurrencyServiceDto
+	{
+		/// <summary>
+		/// Id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Service Name.
+		/// </summary>
+		public string ServiceName { get; set; }
+	}
+}
---
+++ src/GreatCurrency.BLL/Models/ServiceCurrency.cs
@@ -0,0 +1,40 @@
+﻿namespace GreatCurrency.BLL.Models
+{
+	public class ServiceCurrency
+	{
+		/// <summary>
+		/// Srvice Name.
+		/// </summary>
+		public string BankName { get; set; }		
+
+		/// <summary>
+		/// Usd buy currency.
+		/// </summary>
+		public double USDBuyRate { get; set; }
+
+		/// <summary>
+		/// Usd sale currency.
+		/// </summary>
+		public double USDSaleRate { get; set; }
+
+		/// <summary>
+		/// Eur buy currency.
+		/// </summary>
+		public double EURBuyRate { get; set; }
+
+		/// <summary>
+		/// Eur sale currency.
+		/// </summary>
+		public double EURSaleRate { get; set; }
+
+		/// <summary>
+		/// RUB buy currency.
+		/// </summary>
+		public double RUBBuyRate { get; set; }
+
+		/// <summary>
+		/// RUB sale currency.
+		/// </summary>
+		public double RUBSaleRate { get; set; }
+	}
+}
