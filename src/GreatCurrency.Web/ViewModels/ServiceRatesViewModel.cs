662fe046a9429ac1360d24774e4dc2610735a9cb
Ruh <ruh@tut.by>
2024-11-21 13:34:45 +0300

    init: add controller, models and pages for work with service currencies.

---
+++ src/GreatCurrency.Web/ViewModels/ServiceRatesViewModel.cs
@@ -0,0 +1,55 @@
+﻿namespace GreatCurrency.Web.ViewModels
+{
+	public class ServiceRatesViewModel
+	{
+		/// <summary>
+		/// Service currency Id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Usd buy currency.
+		/// </summary>
+		public double USDBuyRate { get; set; }
+
+		/// <summary>
+		/// Usd sale currency.
+		/// </summary>
+		public double USDSaleRate { get; set; }
+
+		/// <summary>
+		/// Eur buy currency.
+		/// </summary>
+		public double EURBuyRate { get; set; }
+
+		/// <summary>
+		/// Eur sale currency.
+		/// </summary>
+		public double EURSaleRate { get; set; }
+
+		/// <summary>
+		/// RUB buy currency.
+		/// </summary>
+		public double RUBBuyRate { get; set; }
+
+		/// <summary>
+		/// RUB sale currency.
+		/// </summary>
+		public double RUBSaleRate { get; set; }
+
+		/// <summary>
+		/// Service Id.
+		/// </summary>
+		public int ServiceId { get; set; }
+
+		/// <summary>
+		/// Service Name.
+		/// </summary>
+		public string ServiceName { get; set; }
+
+		/// <summary>
+		/// Request time.
+		/// </summary>
+		public DateTime RequestTime { get; set; }
+	}
+}
