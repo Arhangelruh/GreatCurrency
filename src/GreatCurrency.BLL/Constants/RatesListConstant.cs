8a660aafbb4ee78ab31828429581f53245bff028
Ruh <ruh@tut.by>
2025-07-07 12:05:25 +0300

    init: add constants: links to api, current currency list.

---
+++ src/GreatCurrency.BLL/Constants/RatesListConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace GreatCurrency.BLL.Constants
+{
+	public class RatesListConstant
+	{
+		public static List<string> Rates = [RatesConstant.USD, RatesConstant.EUR, RatesConstant.RUB];
+	}
+}
