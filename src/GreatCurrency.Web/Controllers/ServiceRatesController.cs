6c2109b732067ad702b1d613c936d2df2fe858d1
Ruh <ruh@tut.by>
2025-07-08 12:05:16 +0300

    init: add UI part for work with services

--- src/GreatCurrency.Web/Controllers/ServiceRatesController.cs
+++ src/GreatCurrency.Web/Controllers/ServiceRatesController.cs
@@ -1,4 +1,6 @@
 ﻿using GreatCurrency.BLL.Interfaces;
+using GreatCurrency.BLL.Models;
+using GreatCurrency.BLL.Services;
 using GreatCurrency.Web.Services;
 using GreatCurrency.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -82,6 +84,171 @@ namespace GreatCurrency.Web.Controllers
 			ViewBag.ErrorMessage = "Ошибка ввода даты.";
 			return View("~/Views/Error/Error.cshtml");
 		}
+
+		/// <summary>
+		/// Get all services.
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public async Task<IActionResult> Services()
+		{
+			var services = await _serviceCurrencyService.GetAllServicesAsync();
+			var models = new List<ServiceViewModel>();
+
+			if (services.Count() != 0)
+			{
+				foreach (var service in services)
+				{
+					models.Add(new ServiceViewModel
+					{
+						Id = service.Id,
+						ServiceName = service.ServiceName						
+					});
+				}
+			}
+			return View(models);
+		}
+
+		/// <summary>
+		/// Model for create service.
+		/// </summary>
+		/// <returns>View</returns>
+		[HttpGet]
+		public IActionResult CreateService()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// Create request.
+		/// </summary>
+		/// <param name="model">Service view model</param>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<IActionResult> CreateService(ServiceViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var services = await _serviceCurrencyService.GetAllServicesAsync();
+				var checkService = services.FirstOrDefault(service => service.ServiceName == model.ServiceName);
+
+				if (checkService == null)
+				{
+					var service = new ServiceDto()
+					{
+						ServiceName = model.ServiceName						
+					};
+
+					await _serviceCurrencyService.AddServiceAsync(service);					
+
+					return RedirectToAction("Services");
+				}
+				else
+				{
+					ModelState.AddModelError("Error", "Такой сервис уже есть в списке.");
+					return View(model);
+				}
+			}
+			return View(model);
+		}
+
+		/// <summary>
+		/// Model for edit.
+		/// </summary>
+		/// <param name="serviceId">Service id</param>
+		/// <returns>View service model</returns>
+		[HttpGet]
+		public async Task<IActionResult> EditService(int serviceId)
+		{
+			var getService = await _serviceCurrencyService.GetServiceByIdAsync(serviceId);
+			if (getService != null)
+			{
+				var serviceViewModel = new ServiceViewModel
+				{
+					Id = getService.Id,
+					ServiceName = getService.ServiceName					
+				};
+				return View(serviceViewModel);
+			}
+			else
+			{
+				ViewBag.ErrorTitle = "Ошибка";
+				ViewBag.ErrorMessage = "Сервис не найден.";
+				return View("~/Views/Error/NotFound.cshtml");
+			}
+		}
+
+		/// <summary>
+		/// Edit service.
+		/// </summary>
+		/// <param name="model">Service view model</param>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<IActionResult> EditService(ServiceViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var service = new ServiceDto
+				{
+					Id = model.Id,
+					ServiceName = model.ServiceName					
+				};
+
+				await _serviceCurrencyService.UpdateServiceAsync(service);
+				return RedirectToAction("Services");
+			}
+			else
+			{
+				return View(model.Id);
+			}
+		}
+
+		/// <summary>
+		/// Get service.
+		/// </summary>
+		/// <param name="serviceId">Service id</param>
+		/// <returns>Service view model</returns>
+		[HttpGet]
+		public async Task<IActionResult> GetService(int serviceId)
+		{
+			var getservice = await _serviceCurrencyService.GetServiceByIdAsync(serviceId);
+			if (getservice != null)
+			{
+				var service = new ServiceViewModel
+				{
+					Id = getservice.Id,
+					ServiceName = getservice.ServiceName					
+				};
+				return View(service);
+			}
+			else
+			{
+				ViewBag.ErrorTitle = "Ошибка";
+				ViewBag.ErrorMessage = "Сервис не найден.";
+				return View("~/Views/Error/NotFound.cshtml");
+			}
+		}
+
+		/// <summary>
+		/// Delete service
+		/// </summary>
+		/// <param name="serviceId"></param>
+		/// <returns>Result</returns>
+		[HttpGet]
+		public async Task<IActionResult> DeleteService(int serviceId)
+		{
+			var service = await _serviceCurrencyService.GetServiceByIdAsync(serviceId);
+			if (service != null)
+			{
+				var deleteResult = await _serviceCurrencyService.DeleteServiceAsync(service);
+				if (deleteResult)
+				{
+					return Json("success");
+				}
+				return Json("error");
+			}
+			return Json("error");
+		}		
 	}
 
 
