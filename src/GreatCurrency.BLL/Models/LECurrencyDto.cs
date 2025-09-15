ad890b17d90b5eb41d634a6c9bb57227961dc2a4
Ruh <ruh@tut.by>
2025-09-15 15:15:33 +0300

    init: add dto models for legal rates

---
+++ src/GreatCurrency.BLL/Models/LECurrencyDto.cs
@@ -0,0 +1,62 @@
+﻿using GreatCurrency.DAL.Models;
+
+namespace GreatCurrency.BLL.Models
+{
+	public class LECurrencyDto
+	{
+		/// <summary>
+		/// Currency Id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Usd buy currency.
+		/// </summary>
+		public double USDBuyRate { get; set; }
+
+		/// <summary>
+		/// Usd sale currency.
+		/// </summary>
+		public double USDSaleRate { get; set; }
+
+		/// <summary>
+		/// Eur buy currency.
+		/// </summary>
+		public double EURBuyRate { get; set; }
+
+		/// <summary>
+		/// Eur sale currency.
+		/// </summary>
+		public double EURSaleRate { get; set; }
+
+		/// <summary>
+		/// RUB buy currency.
+		/// </summary>
+		public double RUBBuyRate { get; set; }
+
+		/// <summary>
+		/// RUB sale currency.
+		/// </summary>
+		public double RUBSaleRate { get; set; }
+
+		/// <summary>
+		/// CNY buy currency.
+		/// </summary>
+		public double CNYBuyRate { get; set; }
+
+		/// <summary>
+		/// CNY sale currency.
+		/// </summary>
+		public double CNYSaleRate { get; set; }
+
+		/// <summary>
+		/// Navigate to currency service.
+		/// </summary>
+		public int OrganisationId { get; set; }		
+
+		/// <summary>
+		/// Navigate to SC request.
+		/// </summary>
+		public int RequestId { get; set; }
+	}
+}
---
+++ src/GreatCurrency.BLL/Models/LERequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace GreatCurrency.BLL.Models
+{
+	public class LERequestDto
+	{
+		// <summary>
+		/// Request id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Request date.
+		/// </summary>
+		public DateTime IncomingDate { get; set; }
+	}
+}
