6e5f2f6b1d031e1df2a70b72344cea5a674d5749
Ruh <ruh@tut.by>
2025-09-16 09:16:32 +0300

    init: added models for status bank API

---
+++ src/GreatCurrency.BLL/Models/StatusBankAPIModels/ErrorInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GreatCurrency.BLL.Models.StatusBankAPIModels
+{
+	/// <summary>
+	/// Result of request.
+	/// </summary>
+	public class ErrorInfo
+	{
+
+		/// <summary>
+		/// Result status.
+		/// </summary>
+		[JsonPropertyName("error")]
+		public string Error { get; set; }
+
+		/// <summary>
+		/// Result description.
+		/// </summary>
+		[JsonPropertyName("errorDescription")]
+		public string ErrorDescription { get; set; }
+	}
+}
---
+++ src/GreatCurrency.BLL/Models/StatusBankAPIModels/StatusBankAPILogin.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GreatCurrency.BLL.Models.StatusBankAPIModels
+{
+	public class StatusBankAPILogin
+	{
+		/// <summary>
+		/// Result status.
+		/// </summary>
+		[JsonPropertyName("errorInfo")]
+		public ErrorInfo ErrorInfo { get; set; }
+
+		/// <summary>
+		/// Session token.
+		/// </summary>
+		public string? sessionToken { get; set; }
+	}
+}
