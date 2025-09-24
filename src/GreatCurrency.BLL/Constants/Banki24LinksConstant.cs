73221a600cd51c313dbcd9b5d284b4dd8154e240
Ruh <ruh@tut.by>
2025-09-24 09:26:46 +0300

    init: add constants for banki24 and statusbank api: links, currencies, organisations.

---
+++ src/GreatCurrency.BLL/Constants/Banki24LinksConstant.cs
@@ -0,0 +1,16 @@
+﻿namespace GreatCurrency.BLL.Constants
+{
+	public class Banki24LinksConstant
+	{
+		/// <summary>
+		/// Link for stockRates.
+		/// </summary>
+		public const string StockLink = "https://banki24.by/exchange/currencymarket";
+
+		/// <summary>
+		/// Common link for rates.
+		/// </summary>
+		public const string LegalRatesLink = "https://banki24.by/kurs/dlya-yurlic?curr=";
+
+	}
+}
