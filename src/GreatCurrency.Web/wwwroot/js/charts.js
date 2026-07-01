586a8c4116f7f4dfa01bb95db9bbc579e942d3fe
Ruh <ruh@tut.by>
2026-07-01 09:56:08 +0300

    init: add chart library

---
+++ src/GreatCurrency.Web/wwwroot/js/charts.js
@@ -0,0 +1,234 @@
+var viewModelData = JSON.parse(document.getElementById('requestModelData').value);
+//var chartsarea =  document.getElementById('USD');
+
+async function showCharts(){        
+    var link = "/Chart/GetStockRates?";   
+    const params = new URLSearchParams(viewModelData);
+    const responce = await fetch(link+params,
+    {
+      method:"Get"
+    })
+    .then(responce => responce.json())
+    responce.forEach(element => {
+      if(element.name == "USD"){
+        const usdChart = new Chart(
+          document.getElementById('USD'),
+           {             
+            type:'line',
+             data: {                            
+              labels: element.list.map(row=>row.time),
+              datasets: [{
+                label: 'Курс продажи',
+                data: element.list.map(row=>row.ourSellRate === 0 ? null : row.ourSellRate ),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам',
+                data: element.list.map(row=>row.dealRate  === 0 ? null : row.dealRate),
+                fill: false,
+                borderColor: 'rgb(235, 52, 140)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс покупки',
+                data: element.list.map(row=>row.ourBuyRate === 0 ? null : row.ourBuyRate),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам +5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate + row.dealRate*0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам -5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate - row.dealRate* 0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              }
+            ]
+             },
+             options: {
+              plugins: {
+                  title: {
+                      display: true,
+                      text: 'График USD курсов'
+                  }
+              }
+            }
+          }
+        );
+      }
+      if(element.name == "EUR"){
+        const usdChart = new Chart(
+          document.getElementById('EUR'),
+            {             
+            type:'line',
+             data: {                            
+              labels: element.list.map(row=>row.time),
+              datasets: [{
+                label: 'Курс продажи',
+                data: element.list.map(row=>row.ourSellRate === 0 ? null : row.ourSellRate ),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам',
+                data: element.list.map(row=>row.dealRate  === 0 ? null : row.dealRate),
+                fill: false,
+                borderColor: 'rgb(235, 52, 140)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс покупки',
+                data: element.list.map(row=>row.ourBuyRate === 0 ? null : row.ourBuyRate),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам +5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate + row.dealRate*0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам -5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate - row.dealRate* 0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              }]
+             },
+             options: {
+              plugins: {
+                  title: {
+                      display: true,
+                      text: 'График EUR курсов'
+                  }
+              }
+            }
+          }
+        );
+      }
+      if(element.name == "RUB"){
+        const usdChart = new Chart(
+          document.getElementById('RUB'),
+            {             
+            type:'line',
+             data: {                            
+              labels: element.list.map(row=>row.time),
+              datasets: [{
+                label: 'Курс продажи',
+                data: element.list.map(row=>row.ourSellRate === 0 ? null : row.ourSellRate*100),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate*100),
+                fill: false,
+                borderColor: 'rgb(235, 52, 140)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс покупки',
+                data: element.list.map(row=>row.ourBuyRate === 0 ? null : row.ourBuyRate*100),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам +5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate*100 + row.dealRate*100* 0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам -5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate*100 - row.dealRate*100* 0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              }]
+             },
+             options: {
+              plugins: {
+                  title: {
+                      display: true,
+                      text: 'График RUB курсов'
+                  }
+              }
+            }
+          }          
+        );
+      }
+    if(element.name == "CNY"){
+        const usdChart = new Chart(
+          document.getElementById('CNY'),
+            {             
+            type:'line',
+             data: {                            
+              labels: element.list.map(row=>row.time),
+              datasets: [{
+                label: 'Курс продажи',
+                data: element.list.map(row=>row.ourSellRate === 0 ? null : row.ourSellRate*10),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate*10),
+                fill: false,
+                borderColor: 'rgb(235, 52, 140)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс покупки',
+                data: element.list.map(row=>row.ourBuyRate === 0 ? null : row.ourBuyRate*10),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам +5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate*10 + row.dealRate*10* 0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              },
+              {
+                label: 'Курс биржи по сделкам -5%',
+                data: element.list.map(row=>row.dealRate === 0 ? null : row.dealRate*10 - row.dealRate*10* 0.05),
+                fill: false,
+                borderColor: 'rgb(79, 19, 192)',
+                tension: 0.1
+              }]
+             },
+             options: {
+              plugins: {
+                  title: {
+                      display: true,
+                      text: 'График CNY курсов'
+                  }
+              }
+            }
+          }
+          
+        );
+      }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", showCharts);
